07e5755296914139bb1342dc0483dc8224259345
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-27 22:27:53 +0600

    0.0.2

--- Method.php
+++ Method.php
@@ -3,11 +3,18 @@ namespace Dfe\ACH;
 // 2019-09-28
 final class Method extends \Df\Payment\Method {
 	/**
-	 * 2019-09-28 The result should be in the basic monetary unit (like dollars), not in fractions (like cents).
+	 * 2019-09-28
 	 * @override
 	 * @see \Df\Payment\Method::amountLimits()
 	 * @used-by \Df\Payment\Method::isAvailable()
 	 * @return \Closure
 	 */
 	protected function amountLimits() {return null;}
+
+	/**
+	 * 2019-09-28
+	 * @used-by \Df\Payment\Method::codeS()
+	 * @see \Df\Payment\Settings::prefix()
+	 */
+	const CODE = 'dfe_ach';
 }
