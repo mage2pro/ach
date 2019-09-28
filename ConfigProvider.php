914be4d7d89f7fdb6c908b755e1e0499e3fbf7ca
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-28 19:36:56 +0600

    0.0.4

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -11,6 +11,11 @@ final class ConfigProvider extends \Df\Payment\ConfigProvider {
 	 * @return array(string => mixed)
 	 */
 	protected function config() {/** @var Settings $s */ $s = $this->s(); return [
+		 /**
+		  * 2019-09-28
+		  * @used-by see \Df\GingerPaymentsBase\ConfigProvider::config()
+		  * https://github.com/mage2pro/ginger-payments-base/blob/1.2.3/ConfigProvider.php#L45
+		  */
 		'desc' => $s->description()
 	] + parent::config();}
 }
