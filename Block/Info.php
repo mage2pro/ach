f54306adb2c210075ed6d398cd77413a8d4d9bbf
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-30 00:44:25 +0600

    no message

--- Block/Info.php
+++ Block/Info.php
@@ -4,6 +4,15 @@ namespace Dfe\ACH\Block;
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Info extends \Df\Payment\Block\Info {
 	/**
+	 * 2019-09-30
+	 * @override
+	 * @see \Df\Payment\Block\Info::msgUnconfirmed()
+	 * @used-by \Df\Payment\Block\Info::rUnconfirmed()
+	 * @return string|null
+	 */
+	final protected function msgUnconfirmed() {return null;}
+
+	/**
 	 * 2019-09-29
 	 * @override
 	 * @see \Df\Payment\Block\Info::prepare()
