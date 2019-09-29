a8ec46c39324396b6e236b41d21ff529c0eca085
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-30 02:30:55 +0600

    no message

--- view/frontend/web/payment-list.js
+++ view/frontend/web/payment-list.js
@@ -2,10 +2,10 @@
  * 2019-09-30
  * Without this mixin, even the standard Magento 2.3.2 checkout
  * does not show my payment methods on a first page load with an empty browser cache.
- * Previously I thought that the problem is only reproducoble with third-party checkout modules.
+ * Previously, I thought that the problem is only reproducible with third-party checkout modules.
  * See my previous evidences of the issue:
- * 1) «How to fix the bug of Aheadworks OneStepCheckout not showing a payment module on the frontend checkout screen?»
- * https://mage2.pro/t/5616
+ * 1) «How to fix the bug of Aheadworks OneStepCheckout not showing a payment module
+ * on the frontend checkout screen?» https://mage2.pro/t/5616
  * 2) «Mageplaza One Step Checkout does not show Mage2.PRO payment methods on the frontend checkout screen»:
  * https://github.com/mage2pro/core/issues/78
  * 3) «Mageplaza One Step Checkout does not show the Stripe module on the frontend checkout screen»:
