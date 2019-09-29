45be44e3e3696c0caaeb9385cdb002f1db49b11d
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-30 01:03:21 +0600

    1.0.0

--- Block/Info.php
+++ Block/Info.php
@@ -1,7 +1,10 @@
 <?php
 namespace Dfe\ACH\Block;
-// 2019-09-29
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+/**
+ * 2019-09-29
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * @method \Dfe\ACH\Method m()
+ */
 class Info extends \Df\Payment\Block\Info {
 	/**
 	 * 2019-09-30
@@ -18,5 +21,18 @@ class Info extends \Df\Payment\Block\Info {
 	 * @see \Df\Payment\Block\Info::prepare()
 	 * @used-by \Df\Payment\Block\Info::prepareToRendering()
 	 */
-	final protected function prepare() {}
+	final protected function prepare() {
+		$this->si(['Routing Number' => $this->m()->routing(), 'Account Number' => $this->m()->account()]);
+	}
+
+	/**
+	 * 2019-09-30
+	 * @override
+	 * @see \Df\Payment\Block\Info::prepareUnconfirmed()
+	 * @used-by \Df\Payment\Block\Info::prepareToRendering()
+	 */
+	final protected function prepareUnconfirmed() {
+		parent::prepareUnconfirmed();
+		$this->prepare();
+	}
 }
